707827d7a4ec5a5671f2520f1233e0b380119b3a
DianaBerte <dianaberte.go@gmail.com>
2023-01-31 15:04:49 +0100

    imported footer correctly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNav from "./components/MyNav";
+import MyFooter from "./components/MyFooter";
 
 function App() {
   return (
     <div>
       <MyNav />
+      <MyFooter />
     </div>
   );
 }
