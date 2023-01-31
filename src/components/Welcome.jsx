89f2450834ee1dac5bd57af7154ea4a2f3ff4382
DianaBerte <dianaberte.go@gmail.com>
2023-01-31 15:44:39 +0100

    jumbotron added

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,17 @@
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import { Container } from 'react-bootstrap';
+
+function Welcome(props) {
+return (
+<Jumbotron fluid>
+  <Container>
+    <h1>Welcome to EpiBooks!</h1>
+    <h5>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus amet vel culpa voluptates, temporibus magni?
+    </h5>
+  </Container>
+</Jumbotron>
+)
+}
+
+export default Welcome
