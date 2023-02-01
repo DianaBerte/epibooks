9b5268f50d047fa781fb92db66a668801b7b54a1
DianaBerte <dianaberte.go@gmail.com>
2023-02-01 18:22:01 +0100

    adds toggle

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,20 +1,29 @@
 import {Card } from "react-bootstrap";
+import { Component } from "react";
 // import props from "../Books/fantasy.json";
 
-const SingleBook = (props) => {
+class SingleBook extends Component {
+    state = {
+      selected: false,
+    }
+  
+
+    render() {
     return (
         <div>
-        <Card style={{width: '12rem'}}>
+        <Card onClick={() => this.setState({ selected: !this.state.selected })}
+        style={{ border: this.state.selected ? '5px solid green' : 'none' }}>
         <Card.Img
         className="d-block w-100"
-        src={props.book.img}
+        src={this.props.book.img}
         alt="First image"/>
         <Card.Body className="d-flex flex-column">
-            <Card.Title><h6>{props.book.title}</h6></Card.Title>
+            <Card.Title><h6>{this.props.book.title}</h6></Card.Title>
         </Card.Body>
         </Card>
         </div>
     )
+}
 }
 
 export default SingleBook
