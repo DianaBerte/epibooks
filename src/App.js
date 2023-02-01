199e9390a11f5477b9fb3022af5ed6f0c05faeda
DianaBerte <dianaberte.go@gmail.com>
2023-02-01 16:20:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import MyNav from "./components/MyNav";
 import MyFooter from "./components/MyFooter";
 import Welcome from "./components/Welcome";
 import AllTheBooks from "./components/AllTheBooks";
+import book from "./Books/fantasy.json";
 
 function App() {
   return (
     <div>
       <MyNav />
       <Welcome />
-      <AllTheBooks SingleBook={SingleBook} />
+      <AllTheBooks SingleBook={book} />
       <MyFooter />
     </div>
   );
