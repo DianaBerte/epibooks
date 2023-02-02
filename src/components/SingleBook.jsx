83a4f06f997cd8f99841ba0d25547bb71976cbcd
DianaBerte <dianaberte.go@gmail.com>
2023-02-02 14:11:08 +0100

    comment area appears

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -5,39 +5,11 @@ import CommentArea from "./CommentArea";
 
 class SingleBook extends Component {
     state = {
-        comments: [],
+        
         selected: false,
     }
 
-    fetchComments = async () => {
-        try {
-            let response = await fetch(
-                'https://striveschool-api.herokuapp.com/api/comments/0316438960',{
-                    headers: {
-                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M5MzJiN2U3MzczODAwMTUzNzQzNzQiLCJpYXQiOjE2NzUzMzkyODEsImV4cCI6MTY3NjU0ODg4MX0.YkFj9nZ1BdbaigHRiklfnWBAA8uV0osFvcB1J0tNJOY"
-            }
-            } 
-            )
-            console.log(response)
-            if (response.ok) {
-                let data = await response.json()
-                console.log(data)
-                this.setState({
-                    comments: data,
-                    selected: true,
-                })
-            } else {
-                this.setState({
-                    selected: false, 
-                })
-            }
-        } catch (error) {
-            console.log(error)
-            this.setState({
-                selected: false,
-            })
-        }
-    }
+
   
 
     render() {
